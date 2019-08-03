366ac4d11141de8a5889e095cf29633414e558b8
Wilson-Lau <38990370+Wilson-Lau@users.noreply.github.com>
2019-08-02 23:41:27 -0700

    formmated first number

--- docs/asg5/sketch.js
+++ docs/asg5/sketch.js
@@ -1,13 +1,7 @@
-
-let isClicked = false;
-
+//https://stackoverflow.com/questions/8935632/check-if-character-is-number
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(240);
-
-    button = createButton('Play');
-    button.position(width - 80, 20);
-    button.mousePressed(onButtonClicked);
 
     midiPlayer = new MidiPlayer();
     midiPlayer.loadMidis("data/midi_files.json", onMIDIsLoaded);
@@ -17,30 +11,67 @@ function draw() {
     midiPlayer.draw();
 }
 
-function onButtonClicked() {
-    isClicked = !isClicked;
-
-    if(isClicked) {
-        // console.log("start");
-        button.elt.innerHTML = "Pause";
-        midiPlayer.start();
-    }
-    else {
-        button.elt.innerHTML = "Play";
-        midiPlayer.pause();
-    }
-}
-
 function onMIDIsLoaded(pianoRolls) {
     // Pick random file to play
     let pianoRoll = random(pianoRolls);
 
-    midiPlayer.setPianoRoll(pianoRoll, tsCallback);
-
     // Encode the piano roll (2D array) as string
     let midiText = midiPlayer.pianoRoll2Text(pianoRoll);
     console.log(midiText);
+    //converts string to L08 markov.js name format
+    let pitch =[];
+    var lengthOfSound =[];
+    var addedToArray= false;
+    var z=0;
+    for(var x = 0; x < midiText.length; x++)
+    {
+      addedToArray= false;
+      if (!isNaN(parseInt(midiText[x], 10))) {
+        // Is a number
+        console.log(parseInt(midiText[x], 10));
+        pitch[z]=(parseInt(midiText[x], 10));
+        console.log(pitch[z]+ ' added to pitch\n');
+        // if next character is a number
+          if (!isNaN(parseInt(midiText[x+1], 10))) {
+            pitch[z]*=10;
+            pitch[z]+=(parseInt(midiText[x+1], 10));
+            console.log(pitch[z]+ ' added 2nd digit to pitch\n');
+            x++;
+            //if the number is 3 digits
+            if (!isNaN(parseInt(midiText[x+1], 10))) {
+              pitch[z]*=10;
+              pitch[z]+=(parseInt(midiText[x+1], 10));
+              console.log(pitch[z]+ ' added 3rd digit to pitch\n');
+              x++;
+            }
+          }
+
+          /*triggers when second argument
+          if(midiText[x]=='_'){
+              lengthOfSound[z]+=midiText[x+1]
+              x++;
+              //if the number is 2 digits
+              if (!isNaN(parseInt(midiText[x+1], 10))) {
+                lengthOfSound[z]*=10;
+                lengthOfSound[z]+=midiText[x+1];
+                x++;
+              }
+              console.log(lengthOfSound[z]+ 'added to lengthOfSound\n');
+        }*/
+
+              //increment to next on pitch and lengthOfSound
+              z++;
+      }
+
+    }
+    /*
+    let pd = train();
+    name = generateName(pd);
+    console.log(pd);
+    */
+    midiPlayer.setPianoRoll(pianoRoll, tsCallback);
 }
+
 
 function tsCallback(currentTs, notesOn) {
     // console.log(currentTs, notesOn);
